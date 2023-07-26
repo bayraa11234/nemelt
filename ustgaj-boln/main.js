6dbde568d2e36446f9a325aaf37e066e27b3f129
bayraa11234 <odbayardorjsuren8@gmail.com>
2023-07-26 17:46:48 +0800

    inputiin utgiig avah

--- ustgaj-boln/main.js
+++ ustgaj-boln/main.js
@@ -1,1 +1,9 @@
 const carsTarget = document.querySelector('#cars');
+const p = document.querySelector('.p');
+const button = document.getElementById('button')
+
+function HandleClick(){
+// document.querySelector('.p').style.marginTop = '100px'
+p.classList.toggle('active')
+}
+button.addEventListener('click',HandleClick)
