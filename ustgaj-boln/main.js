0e9f5e5d9923ad7607bad807d5725f66cb0ad133
bayraa11234 <odbayardorjsuren8@gmail.com>
2023-07-26 19:50:24 +0800

    sdfghj

--- ustgaj-boln/main.js
+++ ustgaj-boln/main.js
@@ -1,6 +1,7 @@
 const carsTarget = document.querySelector('#cars');
 const p = document.querySelector('.p');
 const button = document.getElementById('button')
+const x = document.getElementById["numb"].value;
 
 function HandleClick(){
 // document.querySelector('.p').style.marginTop = '100px'
